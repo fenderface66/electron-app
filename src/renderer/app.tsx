20c970ff759a791ea47f7a2493454004c9371bea
Robbie Heygate <r.heygate@gmail.com>
2019-10-19 13:04:16 +0100

    Addedf oAuth2

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
-import ToolBar from "./ToolBar";
+import GoogleLogin from 'react-google-login';
 
 export class App extends React.Component<undefined, undefined> {
+  responseGoogle(response: any) {
+    console.log(response);
+  }
   render() {
+      console.log(process.env.GOOGLE_CLIENT_ID);
     return (
       <div>
-        <ToolBar />
+        <GoogleLogin
+            clientId={process.env.GOOGLE_CLIENT_ID as string}
+            buttonText="Login"
+            onSuccess={this.responseGoogle}
+            onFailure={this.responseGoogle}
+            cookiePolicy={'single_host_origin'}
+        />
       </div>
     );
   }
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app';
 
+require('dotenv').config()
+
 const render = (Component: any) => {
     ReactDOM.render(
         <Component />,
